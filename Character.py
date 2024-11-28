b985e6f49f69a89e10b3a9e275c52eaa636d62d2
MGergo20 <medgyaszayg3@gmail.com>
2024-11-28 08:52:43 +0100

    Gold, Nevek felrakva

--- Character.py
+++ Character.py
@@ -1,12 +1,14 @@
 import random
 
 class Player:
-    def __init__(self,skill:int=0, hp:int=0, luck:int=0, emo:str="H", poz:int=0):
+    def __init__(self,skill:int=0,Warrior_name:str="FoxRudy", hp:int=0, luck:int=0, emo:str="H", poz:int=0, gold:int=0):
         self.skill=self.dobas_skill()
+        self.Warrior_name=Warrior_name
         self.hp=self.dobas_hp()
         self.luck=self.dobas_luck()
         self.emo=emo
         self.poz=poz
+        self.gold=gold
     
     def dobas_skill(self):
         return random.randint(1,6)+6
@@ -18,13 +20,15 @@ class Player:
         return random.randint(1,6)+6
     
     def __str__(self):
-        return (f"Skill: {self.skill}, HP: {self.hp}, Luck: {self.luck}, Karakter: {self.emo}, Pozíció: {self.poz}")
+        return (f"Skill: {self.skill}, Név: {self.Warrior_name} HP: {self.hp}, Luck: {self.luck}, Karakter: {self.emo}, Pozíció: {self.poz}")
     
 class Enemy:
-    def __init__(self,skill,hp,emo_e:str="E", enemy_poz:int=0):
+    def __init__(self,skill,hp,emo_e:str="E", enemy_poz:int=0, enemy_name:str="Monster"):
         self.skill=skill
         self.hp=hp
         self.emo_e=emo_e
         self.enemy_poz=enemy_poz
+        self.enemy_name=enemy_name
     def __str__(self):
-        return (f"Skill: {self.skill}, HP: {self.hp}, Karakter: {self.emo_e}, Pozíció: {self.enemy_poz}")
+        return (f"Skill: {self.skill}, Név: {self.enemy_name}, HP: {self.hp}, Karakter: {self.emo_e}, Pozíció: {self.enemy_poz}")
+ 
