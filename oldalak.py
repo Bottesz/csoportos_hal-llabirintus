220ea538943bece5043a6b559fb2e84fc05867ed
Polgár Botond (SZF_2023_2025) <polgar.botond@diak.szamalk-szalezi.hu>
2024-11-28 08:35:56 +0100

    oldalak_lépés

---
+++ oldalak.py
@@ -0,0 +1,48 @@
+def Oldalak(self, oldal_szam):
+        
+        if oldal_szam == 1:
+            print("1. oldal:\nEgy kőasztalhoz érsz. Mit teszel?")
+            print("1) Kinyitod a saját neveddel ellátott dobozt (270. oldal).")
+            print("2) Továbbmész észak felé (66. oldal).")
+            return [(270, "Kinyitod a dobozt"), (66, "Továbbmész észak felé")]
+
+        elif oldal_szam == 270:
+            print("270. oldal:\nTalálsz két aranypénzt és egy tanácsot.")
+            self.player.adjust_gold(2)
+            return [(66, "Továbbmész észak felé")]
+
+        elif oldal_szam == 66:
+            print("66. oldal:\nElágazáshoz érsz.")
+            print("1) Nyugat felé mész (293. oldal).")
+            print("2) Kelet felé mész (56. oldal).")
+            return [(293, "Nyugat felé"), (56, "Kelet felé")]
+
+        elif oldal_szam == 56:
+            print("56. oldal:\nEgy akadállyal találkozol.")
+            print("1) Átmászol rajta (373. oldal).")
+            print("2) Kettévágod karddal (215. oldal).")
+            return [(373, "Átmászol"), (215, "Kettévágod karddal")]
+
+        elif oldal_szam == 373:
+            print("373. oldal:\nÁtvergődsz az akadályon.")
+            return [(56, "Kelet felé folytatod az utat.")]
+
+        elif oldal_szam == 215:
+            print("215. oldal:\nMegsebez a spóra, életerőd csökken.")
+            self.player.adjust_health(-2)
+            return [(56, "Kelet felé folytatod az utat.")]
+
+        elif oldal_szam == 293:
+            print("293. oldal:\nÚjabb elágazás.")
+            print("1) Tovább mész nyugatra (137. oldal).")
+            print("2) Észak felé fordulsz (387. oldal).")
+            return [(137, "Nyugatra"), (387, "Észak felé")]
+
+        elif oldal_szam == 387:
+            print("387. oldal:\nEgy Barlangi Emberrel találkozol. Harcolnod kell!")
+            self.fight(Character("Barlangi Ember", 7, 7, 0))
+            return []
+
+        else:
+            print("Oldal nem elérhető.")
+            return []
