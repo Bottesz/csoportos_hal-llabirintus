f537a56706cfeb92cd646f0eb6deaa5c2a4087b5
MGergo20 <medgyaszayg3@gmail.com>
2024-11-28 08:38:15 +0100

    Character fejlesztve

--- Character.py
+++ Character.py
@@ -1,11 +1,12 @@
 import random
 
 class Player:
-    def __init__(self,skill:int=0, hp:int=0, luck:int=0, emo:str="H"):
+    def __init__(self,skill:int=0, hp:int=0, luck:int=0, emo:str="H", poz:int=0):
         self.skill=self.dobas_skill()
         self.hp=self.dobas_hp()
         self.luck=self.dobas_luck()
         self.emo=emo
+        self.poz=poz
     
     def dobas_skill(self):
         return random.randint(1,6)+6
@@ -17,13 +18,13 @@ class Player:
         return random.randint(1,6)+6
     
     def __str__(self):
-        return (f"Skill: {self.skill}, HP: {self.hp}, Luck: {self.luck}, Karakter: {self.emo}")
+        return (f"Skill: {self.skill}, HP: {self.hp}, Luck: {self.luck}, Karakter: {self.emo}, Pozíció: {self.poz}")
     
 class Enemy:
-    def __init__(self,skill,hp,emo_e:str="E"):
+    def __init__(self,skill,hp,emo_e:str="E", enemy_poz:int=0):
         self.skill=skill
         self.hp=hp
         self.emo_e=emo_e
-
+        self.enemy_poz=enemy_poz
     def __str__(self):
-        return (f"Skill: {self.skill}, HP: {self.hp}, Karakter: {self.emo_e}")
+        return (f"Skill: {self.skill}, HP: {self.hp}, Karakter: {self.emo_e}, Pozíció: {self.enemy_poz}")
